96c369137afc01b0406b822d08d8ffa88357c2a1
Gabriel110 <gabrielcorreiadev@gmail.com>
2022-04-27 08:13:55 -0300

    mudando logger

--- LOCAL_STACK/localstack/handler.py
+++ LOCAL_STACK/localstack/handler.py
@@ -7,4 +7,12 @@ def handler(event, context):
   for record in event['Records']:
     LOGGER.info(record['dynamodb'])
     LOGGER.info(record['eventName'])
+    LOGGER.info(record['dynamodb']['NewImage'])
+    LOGGER.info(record['dynamodb']['OldImage'])
+    LOGGER.info('CHAMANDO')
+
+    if(record['dynamodb']['NewImage']['Nome'] != record['dynamodb']['OldImage']['Nome'] ):
+      LOGGER.info("DIFERENTE")
+    else:
+      LOGGER.info("IGUAL")
   return event
