d20ae72fc64d6bdf527150702f7a5320c56f1b7a
Gabriel110 <gabrielcorreiadev@gmail.com>
2022-05-03 16:47:17 -0300

    started

---
+++ serverles_cron_aws/ci/create_sns_sqs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+aws --endpoint-url=http://localhost:4566 sns create-topic --name test-sns
+aws --endpoint-url=http://localhost:4566 sns list-topics
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name test-sqs
+aws --endpoint-url=http://localhost:4566 sqs list-queues
+aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:test-sns --protocol sqs --notification-endpoint http://localhost:4566/000000000000/test-sqs
+aws --endpoint-url=http://localhost:4566 sns list-subscriptions
+aws --endpoint-url=http://localhost:4566 sqs receive-message --queue-url http://localhost:4566/000000000000/test-sqs
---
+++ serverles_cron_aws/ci/serverless_deploy_local.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd ..
+serverless deploy --stage local
+#serverless info --stage local
+exit
