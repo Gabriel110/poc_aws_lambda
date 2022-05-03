bda08f907c8b7a2c18ca00b76cd55c851f5460d3
Gabriel110 <gabrielcorreiadev@gmail.com>
2022-05-03 16:36:48 -0300

    mudando a infra

--- serverless_cron/src/handler.py
+++ serverless_cron/src/handler.py
@@ -1,7 +1,19 @@
 import json
+import boto3
+from boto3.dynamodb.conditions import Key
+import logging
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 
 def handler(event, context):
+    urlDynamon = "http://localhost:4566"
+    dynamodb = boto3.resource('dynamodb', endpoint_url=urlDynamon)
+
+    table = dynamodb.Table('my_table')
+    reponse = table.get_item(Key={'id': '1'})
+    LOGGER.info("Reponse: %s", reponse)
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
